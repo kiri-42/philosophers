a3fe25a1a14ce9b703ec81b5d51c2e0eab5e590c
kiri0314 <t.kirihara0314@gmail.com>
2022-01-10 17:00:35 +0900

    change:return value from int to void

--- philo/include/philo.h
+++ philo/include/philo.h
@@ -6,7 +6,7 @@
 /*   By: tkirihar <tkirihar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/10 15:37:38 by tkirihar          #+#    #+#             */
-/*   Updated: 2022/01/10 15:58:07 by tkirihar         ###   ########.fr       */
+/*   Updated: 2022/01/10 16:58:27 by tkirihar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,6 @@ void	check_arg(int ac, char **av);
 
 /* finish_error */
 
-int		finish_error(char *error_message);
+void	finish_error(char *error_message);
 
 #endif
--- philo/srcs/finish_error.c
+++ philo/srcs/finish_error.c
@@ -6,15 +6,15 @@
 /*   By: tkirihar <tkirihar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/10 15:50:28 by tkirihar          #+#    #+#             */
-/*   Updated: 2022/01/10 15:55:23 by tkirihar         ###   ########.fr       */
+/*   Updated: 2022/01/10 16:57:30 by tkirihar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/philo.h"
 
-int	finish_error(char *error_message)
+void	finish_error(char *error_message)
 {
 	ft_putstr_fd("\x1b[31mError: \x1b[39m", STDERR_FILENO);
 	ft_putendl_fd(error_message, STDERR_FILENO);
-	return (ERROR);
+	exit(ERROR);
 }
