20c131a5a1f33ace66b7ee728afecffbae0d600b
kiri0314 <t.kirihara0314@gmail.com>
2022-01-10 16:10:38 +0900

    upgrade:Create Makefile reles

--- philo/srcs/check_arg.c
+++ philo/srcs/check_arg.c
@@ -6,7 +6,7 @@
 /*   By: tkirihar <tkirihar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/10 15:41:35 by tkirihar          #+#    #+#             */
-/*   Updated: 2022/01/10 15:56:38 by tkirihar         ###   ########.fr       */
+/*   Updated: 2022/01/10 16:08:08 by tkirihar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 void	check_arg(int ac, char **av)
 {
+	(void)av;
 	if (!(ac == 5 || ac == 6))
 		exit(finish_error("the number of options is incorrect"));
 }
