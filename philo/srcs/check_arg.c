a00ae87b3707a03bef4c9aeb6484e00f21e9df8c
kiri0314 <t.kirihara0314@gmail.com>
2022-01-10 17:03:58 +0900

    upgrade:Add check av

--- philo/srcs/check_arg.c
+++ philo/srcs/check_arg.c
@@ -6,7 +6,7 @@
 /*   By: tkirihar <tkirihar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/10 15:41:35 by tkirihar          #+#    #+#             */
-/*   Updated: 2022/01/10 16:08:08 by tkirihar         ###   ########.fr       */
+/*   Updated: 2022/01/10 17:01:59 by tkirihar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,23 @@
 
 void	check_arg(int ac, char **av)
 {
-	(void)av;
+	size_t	ac_i;
+	size_t	char_i;
+
 	if (!(ac == 5 || ac == 6))
-		exit(finish_error("the number of options is incorrect"));
+		finish_error("the number of options is incorrect");
+	ac_i = 1;
+	while (ac_i < (size_t)ac)
+	{
+		char_i = 0;
+		while (av[ac_i][char_i] != '\0')
+		{
+			if (!(ft_isdigit(av[ac_i][char_i])))
+				finish_error("non-numeric character was detected in the options");
+			char_i++;
+		}
+		if (!(check_int(av[ac_i])))
+			finish_error("the option string does not fit in the int range");
+		ac_i++;
+	}
 }
