cacdff73b16c8d85f8370f17a2a3e1a4073f249d
kiri0314 <t.kirihara0314@gmail.com>
2022-01-14 00:11:56 +0900

    clean

--- philo/srcs/put_log.c
+++ philo/srcs/put_log.c
@@ -6,7 +6,7 @@
 /*   By: tkirihar <tkirihar@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/11 15:34:46 by tkirihar          #+#    #+#             */
-/*   Updated: 2022/01/12 22:41:32 by tkirihar         ###   ########.fr       */
+/*   Updated: 2022/01/14 00:03:43 by tkirihar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,12 @@
 
 void	put_log(t_philo_data *philo, int log_num)
 {
+	long long	time_ms;
+
+	time_ms = get_ms(&philo->time);
 	pthread_mutex_lock(&g_mutex_data.put_log_mutex);
 	printf("%s", g_log_message[log_num].color);
-	printf("%lld %d %s\n", get_ms(&philo->time), philo->philo_id, \
+	printf("%lld %d %s\n", time_ms, philo->philo_id, \
 			g_log_message[log_num].message);
 	printf("%s", F_RESET);
 	pthread_mutex_unlock(&g_mutex_data.put_log_mutex);
